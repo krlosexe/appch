e44d8b6b3ee76e5bb5076a0ae98bcba58cf41faa
Leonardo Tapia <barinas.code@gmail.com>
2020-02-25 22:18:47 -0500

    code refactory

--- screens/Login.js
+++ screens/Login.js
@@ -1,170 +1,126 @@
-import React, { Component } from "react";
-import { Linking } from 'expo';
-import { 
-  View,
-  Text,
-  SafeAreaView,
-  Image,
-  ScrollView,
-  Button,
-  Alert,
-  StyleSheet, 
-  TextInput,
-  TouchableOpacity,
-  Textarea,
-  StatusBar,
-  AsyncStorage
-  
-} 
-from "react-native";
+import React, {useState, useEffect} from "react";
+import { View, Text, SafeAreaView, Image, Button, Alert, TextInput, TouchableOpacity, StatusBar, AsyncStorage } from "react-native";
 import * as Font from 'expo-font';
 import styles from './../Styles'
 import { Icon } from 'react-native-eva-icons';
 import axios from 'axios';
 
 
+function LoginScreen(props){
 
-const initialState = {
-  email: '',
-  password: ''
-};
-
-
-
-class Login extends React.Component {
-
-  constructor(props) {
-    super(props)   
-
-    this.onPressLogin = this.onPressLogin.bind(this)
-
-  }
-
- state = initialState;
+	const [ data , setData ] = useState({
+		email : 'Juanriverarq@gmail.com',
+		password : '12345'
+	})
 
 
-  static navigationOptions = { 
-    title: '',
-    headerShown: false,
 
-  };
-  
-  onUsernameChange = email => {
-    this.setState({email});
-  };
+	const [fontsLoaded , setFontsLoaded] = useState(false)
 
-  onPasswordChange = password => {
-    this.setState({password});
-  };
 
-onPressLogin() {
-    
 
-    const {email, password} = this.state;
-    
-    axios.post('http://app.chseguros.com.co/api/auth', {
-      email:"Juanriverarq@gmail.com",
-      password:"12345"
-     })
-      .then(res => {
-        //console.warn(JSON.stringify(res.data))
-        //console.log(res);
-        //console.log(res.data);
-        
-        
-        _storeData = async () => {
-          try {
-            await AsyncStorage.setItem('nombre', 'JUAN');
-          } catch (error) {
-            // Error saving data
-          }
-        };
+	useEffect(()=>{
+		const _loadAssetAsync = async  ()=> {
 
-        this.props.navigation.navigate('HomeScreen');
+			await Font.loadAsync({
+		  		'poppins-regular': require('./../assets/Fonts/Poppins-Regular.ttf'),
+		  		'poppins-semibold': require('./../assets/Fonts/Poppins-SemiBold.ttf'),
+			}) 
+			setFontsLoaded(true) }
+			_loadAssetAsync()
+	},[])
 
-      })
 
-      .catch(function (error) {
-    // handle error
-        Alert.alert ('Error al iniciar sesión','Usuario o contraseña invalida')
 
-        //console.warn('error')
-        //console.log(error);
-        
-  })
+	const _storeData = async () => {
+		try {
+		  await AsyncStorage.setItem('nombre', 'JUAN');
+		  
+		  props.navigation.navigate('HomeScreen');
+
+		} catch (error) {
+			console.warn('Error al escribir en storage')
+		}
+	  };
+
+
+
+	function onChangeDataHandler( e, name ){
+
+		const { text } = e.nativeEvent;
+
+		setData({
+			...data,
+			[name] : text
+		})
+	}
+
+
+
+	function onPressLogin() {
+		axios.post('http://app.chseguros.com.co/api/auth', data ).then(res => {
+			_storeData()
+		  }).catch(error=>{
+			Alert.alert ('Error al iniciar sesión','Usuario o contraseña invalida')			
+	  	})
+	}
+
+
+
+  if(!fontsLoaded)
+  	return  <SafeAreaView style={{flex:1}}> 
+            	<Text>Cargando</Text>
+          	</SafeAreaView>
+
+
+
+  	return <SafeAreaView style={{flex:1}}>
+
+  		<View style={styles.containerlogin}> 
+  			<StatusBar  style={styles.statuslogin} barStyle='dark-content' />
+<View style={styles.logocontenedor}>
+   <Image source={require('./../assets/Img/logo.png')} style={styles.logo} />
+  </View>
+
+
+  <View>
+    <Text style={styles.textotitulo}></Text>
+  </View>
+  <View>
+    <Text style={styles.textodescripcion}>Obtén los beneficios de ser cliente</Text>
+  </View>
+
+
+<View style={styles.inputFondo}>
+<Icon name="person" width={20} height={20} fill="#7E8CBA" />
+	<TextInput placeholder="Número de documento" style={styles.input} 
+		  placeholderTextColor="#8E9AC3" 
+		  name="email" 
+          value={data.email}
+          onChange={(evt)=>onChangeDataHandler(evt, 'email')} />
+
+</View>
+<View style={styles.inputFondo}>
+<Icon name="unlock" width={20} height={20} fill="#7E8CBA" />
+<TextInput secureTextEntry={true} placeholder="Contraseña" style={styles.input} 
+           placeholderTextColor="#8E9AC3" name="password" 
+           value={data.password}
+           onChange={(evt)=>onChangeDataHandler(evt, 'password')}
+           
+/>
+</View>
+<TouchableOpacity >
+<View style={styles.botoncontenedor}>
+    <Button title="Iniciar Sesión" type="" style={styles.boton} color="white" onPress={()=>onPressLogin()} />
+  </View>
+</TouchableOpacity>
+
+</View>
+
+           </SafeAreaView>
 
 }
 
-  componentDidMount() {
-    this._loadAssetAsync()
-  }
-    onPress = () => {
-    alert ('') 
-  }
-
-  async _loadAssetAsync() {
-    await Font.loadAsync({
-      'poppins-regular': require('./../assets/Fonts/Poppins-Regular.ttf'),
-      'poppins-semibold': require('./../assets/Fonts/Poppins-SemiBold.ttf'),
-    });
-
-    this.setState({ fontsLoaded:true })
-  }
-MainView() {
-      const {navigate} = this.props.navigation;
-    return (
-
-      <View style={styles.containerlogin}> 
-          <StatusBar  style={styles.statuslogin} barStyle='dark-content' />
-     <View style={styles.logocontenedor}>
-           <Image source={require('./../assets/Img/logo.png')} style={styles.logo} />
-          </View>
- 
-       
-          <View>
-            <Text style={styles.textotitulo}></Text>
-          </View>
-          <View>
-            <Text style={styles.textodescripcion}>Obtén los beneficios de ser cliente</Text>
-          </View>
 
 
-        <View style={styles.inputFondo}>
-        <Icon name="person" width={20} height={20} fill="#7E8CBA" />
-        <TextInput placeholder="Número de documento" style={styles.input} 
-                  placeholderTextColor="#8E9AC3" name="email" 
-                  value={this.state.email}
-                  onChangeText={this.onUsernameChange}
-
-          />
-        </View>
-        <View style={styles.inputFondo}>
-        <Icon name="unlock" width={20} height={20} fill="#7E8CBA" />
-        <TextInput secureTextEntry={true} placeholder="Contraseña" style={styles.input} 
-                   placeholderTextColor="#8E9AC3" name="password" 
-                   value={this.state.password}
-                   onChangeText={this.onPasswordChange}
-
-        />
-       </View>
-        <TouchableOpacity >
-        <View style={styles.botoncontenedor}>
-            <Button title="Iniciar Sesión" type="" style={styles.boton} color="white" onPress={this.onPressLogin} />
-          </View>
-       </TouchableOpacity>
-        
-      </View>
-    )
-  }
-
-  render() {
-
-    return (
-       <SafeAreaView style={{flex:1}}>
-        { ! this.state.fontsLoaded ? <Text>Cargando</Text> : this.MainView() }
-      </SafeAreaView>
-    );
-  }
-}
-
-export default Login;
+export default LoginScreen;
